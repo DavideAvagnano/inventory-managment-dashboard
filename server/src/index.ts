fa7ccd2e6f8e8c79d8196611ce673510f287c9c4
DavideAvagnano <dav.avagnano@gmail.com>
2024-12-21 13:34:42 +0100

    Created an endpoint in the backend at route /dashboard (without authentication for now). Now I can go in the frontend and make an API call to this endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 /* ROUTE IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
 
 /* SERVER */
 const port = process.env.PORT || 3001;
