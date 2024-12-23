5ffdfa2be2e099564f7006efffbe58a59e4c0178
DavideAvagnano <dav.avagnano@gmail.com>
2024-12-23 11:54:17 +0100

    Dashboard Page and all components of the dashboad

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,5 +1,82 @@
+import { CardPopularProducts } from "@/app/dashboard/card-popular-products";
+import { CardSalesSummary } from "@/app/dashboard/card-sales-summary";
+import { CardPurchaseSummary } from "@/app/dashboard/card-purchase-summary";
+import { CardExpenseSummary } from "@/app/dashboard/card-expense-summary";
+import { CardStat } from "@/app/dashboard/card-stat";
+import {
+  CheckCircle,
+  Package,
+  Tag,
+  TrendingDown,
+  TrendingUp,
+} from "lucide-react";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
+      <CardSalesSummary />
+      <CardPurchaseSummary />
+      <CardExpenseSummary />
+      <CardStat
+        title="Customer & Expenses"
+        primaryIcon={<Package className="text-blue-600 size-6" />}
+        dateRange="22 - 29 October 2023"
+        details={[
+          {
+            title: "Customer Growth",
+            amount: "175.00",
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Expenses",
+            amount: "10",
+            changePercentage: -56,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+      <CardStat
+        title="Dues & Pending Orders"
+        primaryIcon={<CheckCircle className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 October 2023"
+        details={[
+          {
+            title: "Dues",
+            amount: "250.00",
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Pending Orders",
+            amount: "147",
+            changePercentage: -56,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+      <CardStat
+        title="Sales & Discount"
+        primaryIcon={<Tag className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 October 2023"
+        details={[
+          {
+            title: "Sales",
+            amount: "1000.00",
+            changePercentage: 20,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Discount",
+            amount: "200.00",
+            changePercentage: -10,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Dashboard;
