1ad47aa17b204d73ff3ccb76d5226db152fd1b9f
DavideAvagnano <dav.avagnano@gmail.com>
2024-12-24 10:33:05 +0100

    Changed eslint rules to avoid build issues, changed server/package.json for the correct placement of the index.js file in /dist/src during build

--- client/src/redux/redux.tsx
+++ client/src/redux/redux.tsx
@@ -26,13 +26,13 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 /* REDUX PERSISTENCE */
 const createNoopStorage = () => {
   return {
-    getItem(_key: any) {
+    getItem(_key: string): Promise<unknown> {
       return Promise.resolve(null);
     },
-    setItem(_key: any, value: any) {
+    setItem(_key: string, value: unknown): Promise<unknown> {
       return Promise.resolve(value);
     },
-    removeItem(_key: any) {
+    removeItem(_key: string): Promise<void> {
       return Promise.resolve();
     },
   };
